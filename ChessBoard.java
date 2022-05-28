221df1223232778826c99a0fc01b75e52e99fe28
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2022-05-28 20:23:56 +0200

    Primeros comandos

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,3 +1,21 @@
 public class ChessBoard {
+    int dimension;
+    char caracter1;
+    char caracter2;
+    char board;
 
+    ChessBoard(int dimension, char caracter1, char caracter2)
+    {
+        this.dimension = dimension;
+        this.caracter1 = caracter1;
+        this.caracter2 = caracter2;
+
+        board = new char[dimension][dimension];
+
+        for(int i = 1; i < dimension; i++){
+            for(int j = 1; j < dimension; j++){
+                board[i][j] = caracter1;
+            }
+        }
+    }
 }
